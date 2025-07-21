493abd72b864b5d9b950776bba7d40d48878c0e2
Romando Purba <romando@ecampus.ut.ac.id>
2025-07-21 15:55:21 +0700

    save

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	site_name: 'Romando Purba | Portfolio',
+	title: 'Romando Purba | Portfolio',
+	description: 'Portfolio of Romando Purba, a software engineer specializing in backend development.',
+	author: 'Romando Purba',
+	locale_region: 'id-ID',
+	locale: 'id',
+};
